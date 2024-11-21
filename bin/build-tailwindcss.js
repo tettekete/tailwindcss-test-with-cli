0366b020777e04f02ceeedabcd255b5318315bce
tettekete <6535634+tettekete@users.noreply.github.com>
2024-11-22 08:15:45 +0900

    update: bin/build-tailwindcss.js supported build global and individual.
    TailwindCSS for individual HTML files no longer builds unnecessary styles.
    
    global.src.css and tailwind.min.src.css are affected by the use of TailwindCSS
    styles in all HTML files.
    
    *.src.css with the same name as an HTML file only references the CSS for that
     HTML file.

--- bin/build-tailwindcss.js
+++ bin/build-tailwindcss.js
@@ -1,10 +1,40 @@
-// const fs = require('fs');
+const fs = require('fs');
 const path = require('path');
 const chokidar = require('chokidar');
 const { execFile } = require('child_process');
 
+/*
+global.src.css と　tailwind.min.src.css は --content 指定をしない。
+本プロジェクト内で使用されている TailwindCSS スタイルを書きだしてしまって
+構わないので。
+
+	e.g.)
+	taiwindcss -i global.src.css　-o global.css
+
+それ以外の *.src.css は --content で対応する HTML ファイルを指定することで
+全ての HTML 内の TailwindCSS スタイルをスキャンしないように制約する。
+
+	e.g.)
+	taiwindcss -i docs/forms/buttons-like-bootstrap.src.css \
+		-o docs/forms/buttons-like-bootstrap.css\
+		--content docs/forms/buttons-like-bootstrap.html
+
+*/
+
 function buildWithTailwindcss( file_path )
 {
+	let content_option = undefined;
+	switch( path.basename( file_path ) )
+	{
+		case 'global.src.css':
+		case 'tailwind.min.src.css':
+			break;
+
+		default:
+			content_option = file_path.replace( /\.src\.css$/i ,'.html' );
+			break;
+	}
+
 	const dest_file = file_path.replace( /\.src\.css$/i ,'.css' );
 
 	if( dest_file === file_path )
@@ -12,16 +42,28 @@ function buildWithTailwindcss( file_path )
 		return false;
 	}
 
-	console.log(`build: "${file_path}"`);
-	
-	execFile('npx',
-		[
+	const command = [
 			'tailwindcss',
 			'-i',
 			file_path,
 			'-o',
 			dest_file
-		],
+		]
+	
+	if( content_option )
+	{
+		command.push( '--content' );
+		command.push( content_option );
+		
+		console.log(`build: "${file_path}" for ${content_option}`);
+	}
+	else
+	{
+		console.log(`build: "${file_path}" for all HTML`);
+	}
+
+	execFile('npx',
+		command,
 		(error, stdout, stderr) =>
 		{
 			if (error)
@@ -42,12 +84,35 @@ function buildWithTailwindcss( file_path )
 	return true;
 }
 
-const targetDir = process.argv[2] || 'docs';
+const targetDir = process.argv[2] || path.join( __dirname ,'../' , 'docs');
+const allow_ext_list = ['.src.css' , '.html' ,'.js'];
 
 const watcher = chokidar.watch(
 	targetDir,
 	{
-		ignored: (path, stats) => stats?.isFile() && !path.endsWith('.src.css'),
+		ignored: (_path, stats) => {
+			// ファイルじゃない = undefined or directory は無視しない
+			if( ! stats?.isFile() ) {return false }
+
+			// 隠しファイルは無視
+			const basename = path.basename( _path );
+			if( typeof basename === 'string' && basename.startsWith( '.' ) )
+			{
+				return true
+			}
+
+			// 監視対象の拡張子なら無視しない
+			for( const allow_ext of allow_ext_list )
+			{
+				if( _path.endsWith( allow_ext ))
+				{
+					return false;
+				}
+			}
+
+			// それ以外は無視
+			return true;
+		},
 		persistent: true
 	});
 
