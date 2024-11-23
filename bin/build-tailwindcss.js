6739c7a00d0309aa8bafb290248eb9d3f8cea7a6
tettekete <6535634+tettekete@users.noreply.github.com>
2024-11-23 13:23:02 +0900

    update: bin/build-tailwindcss.js buf fix and adjustment execute timing.
    - Fixed a fundamental design error (the updated file and the file that is
    built are not related)
    - Implemented a function to adjust the execution timing (throttling)

--- bin/build-tailwindcss.js
+++ bin/build-tailwindcss.js
@@ -21,7 +21,102 @@ global.src.css と　tailwind.min.src.css は --content 指定をしない。
 
 */
 
-function buildWithTailwindcss( file_path )
+const throttle_sec= 1;
+
+function findFiles( dir ,regex_patterns = [/.+/],ignore_patterns = [] ,found_files = [] )
+{
+	const files = fs.readdirSync(dir);
+
+	files.forEach((file) =>
+	{
+		const filePath = path.join(dir, file);
+		const stat = fs.statSync(filePath);
+
+		if (stat.isDirectory())
+		{
+			findFiles(filePath, regex_patterns , ignore_patterns , found_files );
+		}
+		else
+		{
+			if( ignore_patterns.length > 0 )
+			{
+				for( const regex of ignore_patterns )
+				{
+					if( regex.test( filePath ) )
+					{
+						return;
+					}
+				}
+			}
+
+			for( const regex of regex_patterns )
+			{
+				if( regex.test( filePath ) )
+				{
+					found_files.push( filePath );
+					continue;
+				}
+			}
+		}
+	});
+
+	return found_files;
+}
+
+/*
+	仕様:
+
+	1. 前回の処理実行タイムから throttle_sec 秒以上経過していたら即座に実行し、
+	その後 throttle_sec 秒間のリクエストを破棄する。
+
+	最後の実行から throttle_sec 秒間の間に本当に更新の必要がある呼び出しがあった場合
+	問題になるが、throttle_sec が十分に短い時間に設定すれば作業に支障は出ないはず。
+
+	この仕様にしないと大量のリクエストが一気に来たとき、どうしても初回の即時実行と
+	throttle_sec 秒後の遅延実行の 2 回の実行が必ず発生してしまう。
+	確実性の意味ではこの仕様の方が良いケースもあるが、それならば遅延実行だけにした方が
+	良い事になる。
+
+	ワークフロー的には「throttle_sec 秒以上経過していたら即座に実行する」の方が重要と
+	判断した。
+*/
+function get_throttle_executor( throttle_sec , callback )
+{
+	let _last_dispatch_time = 0;
+	let _restriction_end_time = 0;
+	let _throttle_time = throttle_sec * 1000;
+	let _call_count = 0;
+	
+	return (...args) => {
+		_call_count ++;
+
+		if( Date.now() - _restriction_end_time <= 0  )
+		{
+			return;
+		}
+		else if( Date.now() - _last_dispatch_time > _throttle_time )
+		{
+			_last_dispatch_time		= Date.now();
+			_restriction_end_time	= _last_dispatch_time + _throttle_time;
+
+			console.debug(`call_count: ${_call_count}`);
+			_call_count = 0;
+
+			callback( ...args );
+		}
+	}
+}
+
+
+function buildWithTailwindcss( file_path , srcCssFiles )
+{
+	for(const css_file of srcCssFiles )
+	{
+		execTailwindcssCommand( css_file );
+	}
+}
+
+function execTailwindcssCommand( file_path )
 {
 	let content_option = undefined;
 	switch( path.basename( file_path ) )
@@ -84,38 +179,57 @@ function buildWithTailwindcss( file_path )
 	return true;
 }
 
-const targetDir = process.argv[2] || path.join( __dirname ,'../' , 'docs');
-const allow_ext_list = ['.src.css' , '.html' ,'.js'];
+(()=>
+{
+	// コマンド引数から対象ディレクトリを受け取るかデフォルトパスをセットします
+	const targetDir = process.argv[2] || path.join( __dirname ,'../' , 'docs');
 
-const watcher = chokidar.watch(
-	targetDir,
-	{
-		ignored: (_path, stats) => {
-			// ファイルじゃない = undefined or directory は無視しない
-			if( ! stats?.isFile() ) {return false }
+	// 監視対象となるファイルの拡張子を定義します
+	const allow_ext_list = ['.src.css' , '.html' ,'.js'];
 
-			// 隠しファイルは無視
-			const basename = path.basename( _path );
-			if( typeof basename === 'string' && basename.startsWith( '.' ) )
-			{
-				return true
-			}
+	// tailwindcss ビルド対象ファイルを対象ディレクトリから取り出します。
+	const srcCssFiles = findFiles( targetDir , [/\.src\.css$/] );
 
-			// 監視対象の拡張子なら無視しない
-			for( const allow_ext of allow_ext_list )
-			{
-				if( _path.endsWith( allow_ext ))
+	// chokidar を初期化します
+	const watcher = chokidar.watch(
+		targetDir,
+		{
+			ignored: (_path, stats) => {
+				// ファイルじゃない = undefined or directory は無視しない
+				if( ! stats?.isFile() ) {return false }
+
+				// 隠しファイルは無視
+				const basename = path.basename( _path );
+				if( typeof basename === 'string' && basename.startsWith( '.' ) )
 				{
-					return false;
+					return true
 				}
-			}
 
-			// それ以外は無視
-			return true;
-		},
-		persistent: true
-	});
+				// 監視対象の拡張子なら無視しない
+				for( const allow_ext of allow_ext_list )
+				{
+					if( _path.endsWith( allow_ext ))
+					{
+						return false;
+					}
+				}
 
-watcher
-  .on('add',	(path) => {buildWithTailwindcss(path)})
-  .on('change',	(path) => {buildWithTailwindcss(path)});
+				// それ以外は無視
+				return true;
+			},
+			persistent: true
+		}
+	);
+
+	// 監視対象数に対して実際に処理を行う頻度を抑えるため、スロットリング関数を作ります
+	// (特に chokidar の初回起動時、全てのファイルに add もしくは change イベントが
+	// あったと見なすため、ファイル数と同じ回数同じビルドを行ってしまう。また普通に
+	// 複数のファイル保存を一度に行った場合も同様に同じビルドを複数回行ってしまうのを
+	// 抑えるための処置)
+	const css_builder = get_throttle_executor( throttle_sec , buildWithTailwindcss )
+
+	watcher
+		.on('add',	(path) => {css_builder(path , srcCssFiles )})
+		.on('change',	(path) => {css_builder(path , srcCssFiles )});
+
+})()
